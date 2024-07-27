9c04df926c8bc93d645e7d316229575a64fe8773
CLC <laichwang@gmail.com>
2024-07-27 10:59:53 +0800

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,22 @@
+#============ MAIN =============
+
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+
+# To comment backend block if working on local and terraform state file is locally stored
+  backend "s3" {
+    bucket = "sctp-ce7-tfstate"
+    key    = "terraform-ex-ec2-lcchua.tfstate"
+    region = "us-east-1"
+  }
+}
+
+# Indicate the provider's region
+provider "aws" {
+  region = "us-east-1"
+}
